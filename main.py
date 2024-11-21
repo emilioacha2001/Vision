e1b4f827009a12ad03be5d1821f21f5e41484663
Robot Vision <al189304@alumnos.uacj.mx>
2024-11-21 03:31:19 +0000

    track detection

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 from modules import TrackImage, Robot
-import cv2, time, os
+import cv2, time, os, math
 import numpy as np
 
 robot = Robot()
+robot.tilt(-20)
+direction = 1
+angle = 0
+
 while True:
     frame = robot.cameraCapture()
 
     track = TrackImage(frame)
+    fy, fx, _ = track.frame.shape
+    furthest_line = track.getSmallestCountour(track.lines)
+    if furthest_line is not None and len(furthest_line) > 0:
+        cx, cy = track.getContourCentroid(furthest_line)
+        center_x = fx // 2
+        alpha = math.atan( (cx - center_x) / (fy - cy) )
+        alpha = math.degrees(alpha)
+        robot.pan(alpha/3)
+        robot.turn(alpha)
+        robot.forward(1)
+    elif track.isInsideBorder():
+        robot.turn(90*direction)
+        robot.pan(10*direction)
+        robot.forward(1)
+    else: 
+        robot.stop()
+
     track.drawPeople()
-    track.drawRedLights()
+    track.drawStops()
     track.drawLines()
     cv2.imshow("Test 1", track.frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
+robot.stop()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- modules/TrackImage.py
+++ modules/TrackImage.py
@@ -8,38 +8,25 @@ class TrackImage:
         self.flipped = cv2.flip(self.gray, 1)
         self.hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         self.people = self.detectPeople()
-        self.redLights = self.detectRedLights()
+        self.stops = self.detectStops()
         self.lines = self.detectLines()
 
     def detectPeople(self):
-        rois = self.getROIs([15000, 1000000])
-        face_cascade = cv2.CascadeClassifier("models/haarcascade_fullbody.xml")
-
-        faces = []
-        for roi in rois:
-            x, y, w, h = cv2.boundingRect(roi)
-            roi_faces = face_cascade.detectMultiScale(self.gray[y:y+h, x:x+w], 1.01, 2)
-            if len(roi_faces) == 0:
-                roi_faces = face_cascade.detectMultiScale(self.flipped[y:y+h, x:x+w], 1.01, 2)
-            if len(roi_faces) > 0:
-                roi_faces[:, 0] += x
-                roi_faces[:, 1] += y
-                faces.append(roi_faces[0])
+        people_cascade = cv2.CascadeClassifier("models/people_cascade.xml")
+        people = people_cascade.detectMultiScale(self.frame, 5, 50)
     
-        return faces
+        return people
     
     def drawPeople(self):
         for (x, y, w, h) in self.people:
-            cv2.rectangle(self.frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-    def drawRedLights(self):
-        for contour in self.redLights:
-            x, y, w, h = cv2.boundingRect(contour)
             cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
+    def drawStops(self):
+            for (x, y, w, h) in self.stops:
+                cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
+
     def drawLines(self):
-        for line in self.lines:
-            cv2.rectangle(self.frame, (line[0], line[1]), (line[0]+line[2], line[1]+line[3]), (0, 255, 100), 2)
+        cv2.drawContours(self.frame, self.lines, -1, (122, 255, 122), -1)
 
     def getROIs(self, interest_areas):
         _, thresh = cv2.threshold(self.gray, 220, 255, cv2.THRESH_BINARY)
@@ -48,36 +35,61 @@ class TrackImage:
         cv2.drawContours(self.frame, contours, -1, (255, 0, 0), 2)
         for contour in contours:
             area = cv2.contourArea(contour)
-            print(area)
             if area > interest_areas[0] and area < interest_areas[1]:                
                 valid_contours.append(contour)
 
         return valid_contours
     
-    def detectRedLights(self):
-        lower1 = np.array([0, 205, 30])
-        upper1 = np.array([10, 255, 255])
+    def detectStops(self):
+        cascade = cv2.CascadeClassifier("models/stop_cascade.xml")
+        stops = cascade.detectMultiScale(self.frame, 2.4, 8)
 
-        lower2 = np.array([170, 205, 30])
-        upper2 = np.array([179, 255, 255])
-
-        mask_red_1 = cv2.inRange(self.hsv, lower1, upper1)
-        mask_red_2 = cv2.inRange(self.hsv, lower2, upper2)
-        mask = mask_red_1 + mask_red_2
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        valid_stops = []
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if area > 1000:
-                valid_stops.append(contour)
-
-        return valid_stops
+        return stops
     
     def detectLines(self):
-        rois = self.getROIs([1000, 5000])
+        _, binary = cv2.threshold(self.gray, 40, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
         lines = []
-        for roi in rois:
-            lines.append(cv2.boundingRect(roi))
+        for c in contours:
+            area = cv2.contourArea(c)
+            if area > 1200 and area < 10000:
+                cv2.drawContours(self.frame, [c], -1, (122, 255, 122), -1)
+                lines.append(c)
 
         return lines
+
+    def getSmallestCountour(self, contours):
+        smallest = None
+        for c in contours:
+            if smallest is None or cv2.contourArea(c) < cv2.contourArea(smallest): 
+                smallest = c
+            
+        return smallest
+
+    
+    def getContourCentroid(self, contour):
+        moments = cv2.moments(contour)
+
+        if moments['m00'] != 0:
+            cx = int(moments['m10'] / moments['m00'])
+            cy = int(moments['m01'] / moments['m00'])
+            return (cx, cy)
+        else:
+            return None
+
+    def isInsideBorder(self):
+        low = np.array([35, 20, 20])
+        top = np.array([85, 255, 255])
+
+        mask = cv2.inRange(self.hsv, low, top)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for c in contours:
+            area = cv2.contourArea(c)
+            if area > 2000: 
+                cv2.drawContours(self.frame, [c], -1, (0, 255, 0), -1)  
+                return True
+
+        return False
+
