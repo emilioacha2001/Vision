7f2413cf83c2f8117a5192a2d067254bd2c526b7
Robot Vision <al189304@alumnos.uacj.mx>
2024-11-21 05:54:39 +0000

    Project finished missing calibration for presentation

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import cv2, time, os, math
 import numpy as np
 
 robot = Robot()
-robot.tilt(-20)
+robot.tilt(-10)
 direction = 1
 angle = 0
+start = time.time()
+stopped_time = 0
+stopped = False
 
 while True:
     frame = robot.cameraCapture()
@@ -18,13 +21,35 @@ while True:
         center_x = fx // 2
         alpha = math.atan( (cx - center_x) / (fy - cy) )
         alpha = math.degrees(alpha)
-        robot.pan(alpha/3)
-        robot.turn(alpha)
-        robot.forward(1)
+        duration = stopped_time - start
+
+        if alpha * direction < -5: continue
+        if len(track.people) > 0:
+            robot.stop()
+            continue
+        if len(track.stops) == 1 and not stopped:
+            robot.stop()
+            start = time.time()
+            stopped_time = time.time()
+            stopped = True
+        elif stopped and duration <= 5: 
+            stopped_time = time.time()
+            continue
+        elif stopped and duration <= 8:
+            stopped_time = time.time()
+            robot.pan(alpha/2)
+            robot.turn(alpha/2)
+            robot.forward(1)
+        else:
+            stopped = False
+            robot.pan(alpha/2)
+            robot.turn(alpha/2)
+            robot.forward(1)
+
     elif track.isInsideBorder():
         robot.turn(90*direction)
         robot.pan(10*direction)
-        robot.forward(1)
+        # robot.forward(1)
     else: 
         robot.stop()
 
--- modules/TrackImage.py
+++ modules/TrackImage.py
@@ -13,7 +13,7 @@ class TrackImage:
 
     def detectPeople(self):
         people_cascade = cv2.CascadeClassifier("models/people_cascade.xml")
-        people = people_cascade.detectMultiScale(self.frame, 5, 50)
+        people = people_cascade.detectMultiScale(self.frame, 10, 100)
     
         return people
     
@@ -42,9 +42,15 @@ class TrackImage:
     
     def detectStops(self):
         cascade = cv2.CascadeClassifier("models/stop_cascade.xml")
-        stops = cascade.detectMultiScale(self.frame, 2.4, 8)
+        stops = cascade.detectMultiScale(self.frame, 2, 5)
+        stops = [(x, y, w, h) for (x, y, w, h) in stops if w*h > 10000 and w*h < 40000]
+        valid_stops = []
 
-        return stops
+        for (x, y, w, h) in stops:
+            roi = self.hsv[y:y+h, x:x+w]
+            if self.isObjectRed(roi): valid_stops.append((x, y, w, h))
+
+        return valid_stops
     
     def detectLines(self):
         _, binary = cv2.threshold(self.gray, 40, 255, cv2.THRESH_BINARY)
@@ -53,7 +59,7 @@ class TrackImage:
         lines = []
         for c in contours:
             area = cv2.contourArea(c)
-            if area > 1200 and area < 10000:
+            if area > 1500 and area < 9000:
                 cv2.drawContours(self.frame, [c], -1, (122, 255, 122), -1)
                 lines.append(c)
 
@@ -92,4 +98,22 @@ class TrackImage:
                 return True
 
         return False
+    
+    def isObjectRed(self, img):
+        lower1 = np.array([0, 122, 35])
+        upper1 = np.array([10, 255, 255])
+
+        lower2 = np.array([170, 122, 35])
+        upper2 = np.array([179, 255, 255])
+
+        mask_red_1 = cv2.inRange(img, lower1, upper1)
+        mask_red_2 = cv2.inRange(img, lower2, upper2)
+        mask_red = mask_red_1 + mask_red_2
+        contours, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for c in contours:
+            area = cv2.contourArea(c)
+            if area > 5: return True
+
+        return False
+
 
